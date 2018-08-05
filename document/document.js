83b37d765373574c581c1099d1def1ed447d0744
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-08-04 20:07:49 -0500

    retomando js

---
+++ document/document.js
@@ -0,0 +1,11 @@
+var ruta = window.location;
+document.write("Estás en: " + ruta); //document es un objeto
+console.log("Mensaje secreto uwu.");
+console.log(window.location)
+console.log(window)
+console.log(navigator)
+console.log(console)
+console.log(console.log)
+console.log(document)
+console.log(console.table)
+console.log(console.profile)
