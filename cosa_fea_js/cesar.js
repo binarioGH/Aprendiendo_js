7b3f7dfffe7acdafb841302d72272c20692f1524
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-03-06 23:07:39 -0600

    basura porque no me dio tiempo a terminar

---
+++ cosa_fea_js/cesar.js
@@ -0,0 +1,13 @@
+function cesar(container, key, text){
+	var abc ="abcdefghijklmnopqrstuvwxyz";
+	key = parseInt(key);
+	for(var parser=0; parser<=text.lenght;parser++){
+		if(abc)
+	}
+
+}
+
+var e = document.getElementById("encrypted");
+e.hidden = true;
+
+var 
