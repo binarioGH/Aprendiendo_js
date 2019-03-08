211eff20be20d3b33ee49f6c22b98d52faf84c3c
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-03-07 20:09:56 -0600

    some stupid shit

--- cosa_fea_js/cesar.js
+++ cosa_fea_js/cesar.js
@@ -1,13 +1,39 @@
-function cesar(container, key, text){
+function cesarEncrypt(key, text){
 	var abc ="abcdefghijklmnopqrstuvwxyz";
-	key = parseInt(key);
-	for(var parser=0; parser<=text.lenght;parser++){
-		if(abc)
+	key %= abc.lenght;
+	var l;
+	var x;
+	for(var i=0;i<=text.lenght;i++){
+		var encrypted = "";	
+		l = text[i].toLowerCase;
+		x = abc.indexOf(text[l])
+		if(x>= 0){
+			x -= key;
+			if(x>=abc.lenght){
+				x -= abc.lenght;
+			}
+			else if(x<0){
+				x += abc.lenght
+			}
+			encrypted += abc[x];
+		}
+		else{
+			encrypted += l;
+		}
+		
 	}
-
+	return encrypted;
 }
-
-var e = document.getElementById("encrypted");
-e.hidden = true;
-
-var 
+var getData = document.getElementById("snd");
+getData.addEventListener("click", function(){
+	var t = document.getElementById("cipherText");
+	var k = document.getElementById("key");
+	var t = document.getElementById("text");
+	k.value = parseInt(k.value);
+	if(k.value == NaN){
+		alert("The key has to be a number.");
+		return 0;
+	}
+	t.innerHTML = "Encrypted text: " + cesarEncrypt(k.value, text);
+	console.log(cesarEncrypt(3, "hola mundo"));
+});
